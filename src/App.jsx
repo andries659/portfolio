367c53b83196ac8164b1ec3183e671a37692cba0
AndriesWorks <168019743+andries659@users.noreply.github.com>
2025-06-17 20:25:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Portfolio from './components/Portfolio';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white pt-28">
       <header className="text-center py-10">
         <h1 className="text-4xl font-bold">.angel_xd.'s Project Portfolio</h1>
         <p className="text-gray-600 dark:text-gray-400">
